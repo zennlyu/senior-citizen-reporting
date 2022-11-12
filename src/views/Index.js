138ef3e8af7fd5f498c2fbe149b16ac79d7ccce2
zennlyu <3598267954@qq.com>
2022-11-12 17:12:32 +0800

    .gitignore config

--- src/views/Index.js
+++ src/views/Index.js
@@ -6,7 +6,7 @@ import React from "react";
 // index sections
 import SectionTypography from "./index-sections/SectionTypography";
 import RegisterPage from "./ch-options/RegisterPage";
-import IndexNavbar from "../components/Navbars/IndexNavbar";
+import IndexNavbar from "../components/navbars/indexNavbar";
 
 function Index() {
   document.documentElement.classList.remove("nav-open");
